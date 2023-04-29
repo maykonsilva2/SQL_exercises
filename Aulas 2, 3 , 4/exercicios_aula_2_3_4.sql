fa32e175cad6044e1819f7d56114000987389723
Antonio Michael <mayconvdl@hotmail.com>
2023-04-29 15:07:36 -0300

    Add exercices from lessons 2, 3, 4, 6 and 7

---
+++ Aulas 2, 3 , 4/exercicios_aula_2_3_4.sql
@@ -0,0 +1,13 @@
+--Challenge 01
+/*SELECT Name
+FROM Production.Product 
+WHERE weight >= 500 and weight <= 700;*/
+
+--challenge 02
+/*SELECT * 
+FROM HumanResources.Employee 
+WHERE MaritalStatus = 'M' AND  SalariedFlag = 'True'; */
+
+--Challenge 03
+/*SELECT * FROM Person.Person WHERE FirstName='Peter' AND LastName='Krebs';
+SELECT * FROM Person.EmailAddress WHERE BusinessEntityID = 26;*/
---
+++ Aulas 5 , 6 , 7/exercicios_count.sql
@@ -0,0 +1,8 @@
+--Challenge 01
+--SELECT COUNT(*) FROM Production.Product;
+
+--Challenge 02
+--SELECT COUNT(Size) FROM Production.Product;
+
+--Challenge 03
+--SELECT COUNT(DISTINCT Size) FROM Production.Product;
