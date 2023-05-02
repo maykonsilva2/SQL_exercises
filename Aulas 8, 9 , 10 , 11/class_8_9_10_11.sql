a6d37a00f3906b97a9e4027be2f14da37f6822c7
Antonio Michael <mayconvdl@hotmail.com>
2023-05-01 22:07:34 -0300

    add exercices from lessons 8, 9, 10, 11

---
+++ Aulas 8, 9 , 10 , 11/class_8_9_10_11.sql
@@ -0,0 +1,18 @@
+--Challenge 1
+--SELECT COUNT(ListPrice) FROM Production.Product WHERE ListPrice > 1500;
+
+--challenge 2
+--SELECT COUNT(LastName) FROM Person.Person WHERE LastName LIKE 'P%';
+
+--challenge 3
+--SELECT COUNT(DISTINCT City) FROM Person.Address;
+
+--challenge 4
+--SELECT	DISTINCT (City) FROM  Person.Address;
+
+--CHALLENGE 5
+--SELECT * FROM Production.Product;
+--SELECT COUNT(*) FROM Production.Product WHERE Color='Red' AND  ListPrice BETWEEN 500 AND 1000;
+
+--CHALLENGE 6
+--SELECT COUNT(*) FROM Production.Product WHERE Name LIKE '%road%';
